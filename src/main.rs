65cd4ce5a15496bd2542dee959bae50c32169c29
Landon Call <landoncall@Landons-MacBook-Pro.local>
2020-12-02 17:23:07 -0700

    Higher order functions

--- src/main.rs
+++ src/main.rs
@@ -79,8 +79,47 @@ fn closures() {
      println!("f = {}", f);
 }
 
+// Higher Order Functions
+fn is_even(x: u32) -> bool {
+    x % 2 == 0
+}
+
+fn greater_than(limit: u32) -> impl Fn(u32) -> bool {
+    move |y| y > limit
+}
+
+fn higher() {
+    let limit = 500;
+    let mut sum = 0;
+
+    // let above_limit = |y| y > limit;
+    let above_limit = greater_than(limit);
+
+    for i in 0.. {
+        let isq = i*i;
+
+        //if isq > limit {
+        if above_limit(isq) {
+            break;
+        }
+        else if is_even(isq) {
+            sum += isq;
+        }
+    }
+
+    println!("loop sum = {}", sum);
+
+    let sum2 = (0..)
+        .map(|x| x*x)
+        .take_while(|&x| x < limit)
+        .filter(|x:&u32| is_even(*x))
+        .fold(0, |sum, x| sum + x);
+    println!("hof sum = {}", sum2);
+}
+
 fn main() {
     //functions();
     //methods();
-    closures();
+    //closures();
+    higher();
 }
